fe348897d38940236efaddc5595843f81aaab14b
Fabio Volpe <fabiovolpe29@gmail.com>
2022-05-25 12:05:29 +0200

    Aggiornamento con ricerca di serie(non esistono)

--- js/modulo-api.js
+++ js/modulo-api.js
@@ -1,11 +1,26 @@
 
 const URL_Tafanos = "https://omdbapi.com/?apikey=fb7170de&s=tafanos";
 
+
+//funzione che chiama la web api con l'elenco di tutti i contenuti di "Tafanos"
 export const listTafanos = () => {
     fetch(URL_Tafanos)
     .then((response) => response.json())
     .then((results) => {
         const movies = results.Search;
         console.log(movies);
+    });
+};
+
+//funzione che chiama la web api e restituisce solo le serie televisive che rispondono al nome di "Tafanos"
+
+const URL_Tafanos_Serie = "https://omdbapi.com/?apikey=fb7170de&s=tafanos&type=series";
+
+export const listTafanosSerie = () => {
+    fetch(URL_Tafanos_Serie)
+    .then((responseSerie) => responseSerie.json())
+    .then((resultsSerie) => {
+        const Serie = resultsSerie.Search;
+        console.log(Serie);
     });
 };
