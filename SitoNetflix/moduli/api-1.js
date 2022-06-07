a53c45d0ec0fe8300edfd8e558e28b2a82bf0f9a
Fabio Volpe <fabiovolpe29@gmail.com>
2022-06-07 18:40:04 +0200

    modifcihe varie per migliore graficamente il sito

--- SitoNetflix/moduli/api-1.js
+++ SitoNetflix/moduli/api-1.js
@@ -1,33 +1,4 @@
-/*
-const URL_Tafanos = "https://omdbapi.com/?apikey=fb7170de&s=tafanos";
-
-
-//funzione che chiama la web api con l'elenco di tutti i contenuti di "Tafanos"
-export const listTafanos = () => {
-    fetch(URL_Tafanos)
-    .then((response) => response.json())
-    .then((results) => {
-        const movies = results.Search;
-        console.log(movies);
-    });
-};
-
-//funzione che chiama la web api e restituisce solo le serie televisive che rispondono al nome di "Tafanos"
-
-const URL_Tafanos_Serie = "https://omdbapi.com/?apikey=fb7170de&s=tafanos&type=series";
-
-export const listTafanosSerie = () => {
-    fetch(URL_Tafanos_Serie)
-    .then((responseSerie) => responseSerie.json())
-    .then((resultsSerie) => {
-        const Serie = resultsSerie.Search;
-        console.log(Serie);
-    });
-};
-*/
-
-
-import { BASE_URL} from "./config.js";
+import { BASE_URL} from "./configurazion.js";
 
 export const apiList = (s, type) => {
     const url = BASE_URL + `s=${s}&type=${type}`;
@@ -40,17 +11,7 @@ export const apiList = (s, type) => {
         viewItems(items);
     });
 };
-/*
-const viewItems = (items) =>{
-    items.map((item)=> {
-        console.group(item.Title);
-        console.log(item.Year);
-        console.log(item.Type);
-        console.log(item.Poster);
-        console.groupEnd();
-    });
-};
-*/
+
 const viewItems = (items) =>{
     //posiziono dove voglio l'elenco dei movies
     const element = document.getElementById("movies");
@@ -61,23 +22,7 @@ const viewItems = (items) =>{
         element.appendChild(crateHTMLMovie(item));
     } );
 };
-/*
 
-const crateHTMLMovie = (movie) => {
-    //creo il tag p vuoto
-    const para = document.createElement("p");
-    const image = document.createElement("img");
-    //creo un testo con il titolo del nome del film
-
-    const node = document.createTextNode(movie.Poster);
-    console.log(movie.Poster);
-    const node2 = document.createTextNode(movie.Poster);
-    //inserisco dentro il tag p il testo
-    para.appendChild(node);
-    image.appendChild(node2);
-};
-
-*/
    /**************  PROVIAMO AD INSERIRE UN IMMAGINE    **************/
    const create = (movie) => {
     const element = document.getElementById("card-titolo-1");
@@ -91,7 +36,7 @@ const crateHTMLMovie = (movie) => {
     h5.appendChild(node);
 
     const para = document.createElement("p");
-    const node2 = document.createTextNode(movie.Plot);
+    const node2 = document.createTextNode(movie.Type);
     para.appendChild(node2);
 
     element.replaceChild(h5, titolo_vecchio);
@@ -149,7 +94,7 @@ const secondaFetch = (listaFilm) =>{
 }
 
 const traduciTitolo = (titolo) => {
-    const stringa = titolo.replace(/\s/g, "%20");  // Trasforma gli gli spazzzzi in %20 per ricerca migliore
+    const stringa = titolo.replace(/\s/g, "%20");  // Trasforma gli spazzzzi in %20 per ricerca migliore
     return stringa;
 } 
 
---
+++ SitoNetflix/moduli/configurazion.js
@@ -0,0 +1,11 @@
+
+
+const API_KEY = "fb7170de" ;
+export const BASE_URL = `https://omdbapi.com/?apikey=${API_KEY}&`;
+
+export const CONTENT_TYPE = {
+    MOVIE: "movie",
+    SERIES: "series",
+    EPISODE: "episode",
+    GAME: "game"
+}
